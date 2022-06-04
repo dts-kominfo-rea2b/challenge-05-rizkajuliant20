27545cc2d0b8afb5beef48a010f03f4248f5cdec
rizkajuliant20 <rizkariz20@gmail.com>
2022-06-04 23:59:29 +0700

    first commit for fifth challenge

--- index.js
+++ index.js
@@ -6,25 +6,37 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arr, sortingan) => {
+    const newArr = []
+    let counter = 1
+    sortingan(arr)
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(counter++ + ". " + arr[i])
+    }
+    return newArr
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr) => {
+    return arr.sort()
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) => {
+    return arr.reverse()
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
-  console.log(sorter?.(names, sortAscending)?.join("\n"));
-  console.log(sorter?.(names, sortDescending)?.join("\n"));
+    console.log(sorter(names, sortAscending).join("\n"));
+    console.log(sorter(names, sortDescending).join("\n"));
 })();
 
 module.exports = {
-  sorter,
-  sortAscending,
-  sortDescending,
-  names,
+    sorter,
+    sortAscending,
+    sortDescending,
+    names,
 };
